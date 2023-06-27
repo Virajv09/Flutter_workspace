22606201fe6279806e2ee6464d7020dc6a5dc5c3
Virajv09 <132914057+Virajv09@users.noreply.github.com>
2023-06-27 15:25:39 +0530

    Add files via upload

---
+++ test/area_of_tri.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  int a=4;
+  int b=3;
+
+  var area = a*b/2;
+  print(area);
+}
---
+++ test/subjects.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int s1=40;
+  int s2=70;
+  int s3=80;
+  int s4=100;
+  int s5=90;
+
+  int total=500;
+
+  int sum = s1+s2+s3+s4+s5;
+  print(sum);
+
+  double per=(sum*100)/ total;
+  print(per);
+}
