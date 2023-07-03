83784b5e80b9e2f8157d705d8a2795b56e2708e2
Virajv09 <132914057+Virajv09@users.noreply.github.com>
2023-07-03 16:32:26 +0530

    7.reverse.dart

--- 28-6/7.reverse.dart
+++ 28-6/7.reverse.dart
@@ -1,16 +1,21 @@
 import 'dart:io';
 
-void main()
-{
-  double reverse =0;
+void main() {
+  double reverse = 0;
   print("Enter number for reverse");
-  double a =double.parse(stdin.readLineSync()!);
+  double a = double.parse(stdin.readLineSync()!);
 
-  while(a>0)
+  while (a>= 1)
+  {
+    if (a < 1)
+      reverse = a;
+    else
     {
-      double b = a % 10;
-      reverse = reverse * 10 + b;
-      a /= 10;
+      reverse = a % 10;
+
+      double b = a * 10 + reverse;
+      a = a / 10;
     }
     print(reverse);
+  }
 }
