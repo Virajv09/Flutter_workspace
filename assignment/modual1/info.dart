bbd34b46660792d0099b80bd7c66cff0294ac142
Virajv09 <132914057+Virajv09@users.noreply.github.com>
2023-07-10 18:21:07 +0530

    Add files via upload

---
+++ assignment/modual1/info.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main()
+{
+  print("viraj vekariya");
+  print("14-05-2002");
+  print("21");
+  print("upleta");
+
+
+  String name = stdin.readLineSync().toString();
+  print(name);
+}
